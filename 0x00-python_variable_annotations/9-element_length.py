3f6df4b3b8b0bb526a6b4af7a32768dbbd8ec1a9
ofperez-TheOwl <odidiperez@gmail.com>
2023-01-17 18:40:23 +0000

    correct sequence object importation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,9 +4,9 @@
 """
 
 
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[Sequence]) -> List[Tuple]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """element length"""
     return [(i, len(i)) for i in lst]
